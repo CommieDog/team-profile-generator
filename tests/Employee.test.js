60bbddb620679abb3c216140126327b78e1f2f63
John Netzel <johnrnetzel@gmail.com>
2022-04-22 18:48:25 -0800

    Add test for Employee.name property

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,16 @@
-describe("Test test", () => {
-    it("It just works!", () => {
-        expect(true).toEqual(true);
+const Employee = require("../lib/employee")
+
+describe("Testing Employee class", () =>
+{
+    describe("Properties", () =>
+    {
+        it("Name should be set by constructor", () =>
+        {
+            const employeeName = "John"
+
+            const employee = new Employee(employeeName, 0, "me@me.com");
+
+            expect(employee.name).toEqual(employeeName);
+        })
     })
-})
+});
