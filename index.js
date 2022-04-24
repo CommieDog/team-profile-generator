6b15b8afdb0d495618e39885ab95ca8a0d1d8502
John Netzel <johnrnetzel@gmail.com>
2022-04-24 01:47:36 -0800

    Convert manager info prompt into function

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const Manager = require("./lib/manager");
+
+const team = [];
+
+const managerQuestions = [
+    {
+        name: "managerName",
+        message: "What is the team manager's name?"
+    },
+    {
+        name: "managerId",
+        message: "What is the team manager's employee ID?"
+    },
+    {
+        name: "managerEmail",
+        message: "What is the team manager's email address?"
+    },
+    {
+        name: "managerOfficeNumber",
+        message: "What is the team manager's office number?"
+    }
+];
+
+function promptForManager()
+{
+    return new Promise((resolve, reject) =>
+    {
+        inquirer.prompt(managerQuestions).then((answers) =>
+        {
+            team.push(new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber));
+            resolve();
+        })
+    });
+}
 
 console.log("Welcome to the team profile generator!");
-inquirer.prompt(
-    [
-        {
-            name: "managerName",
-            message: "What is the team manager's name?"
-        },
-        {
-            name: "managerId",
-            message: "What is the team manager's employee ID?"
-        },
-        {
-            name: "managerEmail",
-            message: "What is the team manager's email address?"
-        },
-        {
-            name: "managerOfficeNumber",
-            message: "What is the team manager's office number?"
-        }
-    ]
-).then((answers) =>
+promptForManager().then(() =>
 {
-    let managerAnswers = answers;
+    //let managerAnswers = answers;
     inquirer.prompt(
         [
             {
                 type: "list",
                 name: "nextAction",
                 message: "What you you like to do next?",
-                choices: ["Add an engineer", "Add an intern", "finish and generate profile"]
+                choices: ["Add an engineer", "Add an intern", "Finish and generate profile"]
             }
         ]
     ).then((answers) => 
@@ -43,6 +58,7 @@ inquirer.prompt(
                 console.log("Add an intern");
                 break;
         }
-    console.log(managerAnswers);
+    console.log("Done!");
+    console.log(team);
     })
-})
+});
